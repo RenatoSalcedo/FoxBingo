137d7d26a497adab6ead0ab4a4bcdcfe687ce107
Renato Salcedo <renato_salcedo@hotmail.com>
2017-05-04 11:14:08 -0300

    teste de criação do painel.

--- app/src/main/java/br/com/company/darkfox/foxbingo/MainActivity.java
+++ app/src/main/java/br/com/company/darkfox/foxbingo/MainActivity.java
@@ -2,12 +2,64 @@ package br.com.company.darkfox.foxbingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TableLayout tbBingo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tbBingo = (TableLayout) findViewById(R.id.tbBingo);
+        populatePainel();
+    }
+
+    private void populatePainel()
+    {
+        for(int i = 0; i <= 15; i++)
+        {
+            TableRow tRow = new TableRow(this);
+            tbBingo.addView(tRow);
+        }
+
+        for (int i = 1; 1 <= 15; i++)
+        {
+            int aux = i;
+            for(int j = 1; j < tbBingo.getChildCount(); j++)
+            {
+                TableRow tr = (TableRow) tbBingo.getChildAt(j);
+
+                TextView tView = new TextView(this);
+                tView.setText(aux);
+                tView.setGravity(Gravity.CENTER);
+                tView.setPadding(3, 3, 3, 3);
+
+                TextView tView1 = new TextView(this);
+                tView.setText(aux += 15);
+                tView.setGravity(Gravity.CENTER);
+                tView.setPadding(3, 3, 3, 3);
+
+                TextView tView2 = new TextView(this);
+                tView.setText(aux += 15);
+                tView.setGravity(Gravity.CENTER);
+                tView.setPadding(3, 3, 3, 3);
+
+                TextView tView3 = new TextView(this);
+                tView.setText(aux += 15);
+                tView.setGravity(Gravity.CENTER);
+                tView.setPadding(3, 3, 3, 3);
+
+                TextView tView4 = new TextView(this);
+                tView.setText(aux += 15);
+                tView.setGravity(Gravity.CENTER);
+                tView.setPadding(3, 3, 3, 3);
+            }
+        }
     }
 }
