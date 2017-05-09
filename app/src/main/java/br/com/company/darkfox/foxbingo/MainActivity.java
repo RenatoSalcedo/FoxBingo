42f56640dd79ff7ce106b101b75872c56a186d0f
Renato Salcedo <renato_salcedo@hotmail.com>
2017-05-09 14:32:52 -0300

    ajustes do painel.

--- app/src/main/java/br/com/company/darkfox/foxbingo/MainActivity.java
+++ app/src/main/java/br/com/company/darkfox/foxbingo/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.company.darkfox.foxbingo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -22,44 +25,59 @@ public class MainActivity extends AppCompatActivity {
 
     private void populatePainel()
     {
-        for(int i = 0; i <= 15; i++)
-        {
+        for(int i = 1; i <= 15; i++) {
             TableRow tRow = new TableRow(this);
-            tbBingo.addView(tRow);
-        }
+            tRow.setVisibility(View.VISIBLE);
+            TableLayout.LayoutParams tableRowParams=
+                    new TableLayout.LayoutParams
+                            (TableLayout.LayoutParams.WRAP_CONTENT,TableLayout.LayoutParams.WRAP_CONTENT);
 
-        for (int i = 1; 1 <= 15; i++)
-        {
+            tableRowParams.setMargins(170, 0, 0, 0);
+            tRow.setLayoutParams(tableRowParams);
             int aux = i;
-            for(int j = 1; j < tbBingo.getChildCount(); j++)
-            {
-                TableRow tr = (TableRow) tbBingo.getChildAt(j);
 
-                TextView tView = new TextView(this);
-                tView.setText(aux);
-                tView.setGravity(Gravity.CENTER);
-                tView.setPadding(3, 3, 3, 3);
+            TextView tView = new TextView(this);
+            tView.setText(Integer.toString(aux));
+            tView.setGravity(Gravity.CENTER);
+            tView.setTextSize(26);
+            tView.setPadding(3, 3, 3, 3);
+            tView.setVisibility(View.VISIBLE);
 
-                TextView tView1 = new TextView(this);
-                tView.setText(aux += 15);
-                tView.setGravity(Gravity.CENTER);
-                tView.setPadding(3, 3, 3, 3);
+            TextView tView1 = new TextView(this);
+            tView1.setText(Integer.toString(aux += 15));
+            tView1.setGravity(Gravity.CENTER);
+            tView1.setPadding(3, 3, 3, 3);
+            tView1.setTextSize(26);
+            tView1.setVisibility(View.VISIBLE);
 
-                TextView tView2 = new TextView(this);
-                tView.setText(aux += 15);
-                tView.setGravity(Gravity.CENTER);
-                tView.setPadding(3, 3, 3, 3);
+            TextView tView2 = new TextView(this);
+            tView2.setText(Integer.toString(aux += 15));
+            tView2.setGravity(Gravity.CENTER);
+            tView2.setPadding(3, 3, 3, 3);
+            tView2.setTextSize(26);
+            tView2.setVisibility(View.VISIBLE);
 
-                TextView tView3 = new TextView(this);
-                tView.setText(aux += 15);
-                tView.setGravity(Gravity.CENTER);
-                tView.setPadding(3, 3, 3, 3);
+            TextView tView3 = new TextView(this);
+            tView3.setText(Integer.toString(aux += 15));
+            tView3.setGravity(Gravity.CENTER);
+            tView3.setPadding(3, 3, 3, 3);
+            tView3.setTextSize(26);
+            tView3.setVisibility(View.VISIBLE);
 
-                TextView tView4 = new TextView(this);
-                tView.setText(aux += 15);
-                tView.setGravity(Gravity.CENTER);
-                tView.setPadding(3, 3, 3, 3);
-            }
+            TextView tView4 = new TextView(this);
+            tView4.setText(Integer.toString(aux += 15));
+            tView4.setGravity(Gravity.CENTER);
+            tView4.setPadding(3, 3, 3, 3);
+            tView4.setTextSize(26);
+            tView4.setVisibility(View.VISIBLE);
+
+            tRow.addView(tView);
+            tRow.addView(tView1);
+            tRow.addView(tView2);
+            tRow.addView(tView3);
+            tRow.addView(tView4);
+
+            tbBingo.addView(tRow);
         }
     }
 }
